4a25567f7124f7a21b8ae081c2a87b5dbd401214
tctrautman <tctrautman@gmail.com>
2019-04-02 13:57:56 -0700

    Add User component for checking if a user is signed in

--- components/Nav.js
+++ components/Nav.js
@@ -1,26 +1,34 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
 
 const Nav = () => (
-    <div>
-        <NavStyles>
-            <Link href="/items">
-                <a>Shop</a>
-            </Link>
-            <Link href="/sell">
-                <a>Sell</a>
-            </Link>
-            <Link href="/signup">
-                <a>Signup</a>
-            </Link>
-            <Link href="/orders">
-                <a>Orders</a>
-            </Link>
-            <Link href="/me">
-                <a>Account</a>
-            </Link>
-        </NavStyles>
-    </div>
+    <NavStyles>
+        <User>
+            {({ data: { me } }) => {
+                console.log(me);
+                if (me) {
+                    return <p>{me.name}</p>
+                }
+                return null;
+            }}
+        </User>
+        <Link href="/items">
+            <a>Shop</a>
+        </Link>
+        <Link href="/sell">
+            <a>Sell</a>
+        </Link>
+        <Link href="/signup">
+            <a>Signup</a>
+        </Link>
+        <Link href="/orders">
+            <a>Orders</a>
+        </Link>
+        <Link href="/me">
+            <a>Account</a>
+        </Link>
+    </NavStyles>
 );
 
 export default Nav;
