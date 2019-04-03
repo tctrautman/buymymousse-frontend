567124a4ff65916ef7fb4fe26f89fc9f4ecc2ca6
tctrautman <tctrautman@gmail.com>
2019-04-03 13:50:37 -0700

    Update local cache once an item is removed, set optimistic response for faster loading

--- components/RemoveFromCart.js
+++ components/RemoveFromCart.js
@@ -27,11 +27,39 @@ class RemoveFromCart extends React.Component {
     static propTypes = {
         id: propTypes.string.isRequired
     }
+
+    // This gets called as soon as we get a resp from server
+    // after a mutation has been performed
+    update = (cache, payload) => {
+        console.log('Running remove from cart update function')
+        // First, read the cache
+        const data = cache.readQuery({
+            query: CURRENT_USER_QUERY
+        });
+        console.log(data);
+        // Remove that item from the cart
+        const cartItemId = payload.data.removeFromCart.id;
+        data.me.cart = data.me.cart.filter(cartItem => (
+            cartItem.id !== cartItemId));
+        // Write it back to the cache
+        cache.writeQuery({
+            query: CURRENT_USER_QUERY, data
+        });
+    }
+
     render() {
         return (
             <Mutation 
                 mutation={REMOVE_FROM_CART_MUTATION}
                 variables={{id: this.props.id}}
+                update={this.update}
+                optimisticResponse={{
+                    __typename: 'Mutation',
+                    removeFromCart: {
+                        __typename: 'CartItem',
+                        id: this.props.id
+                    }
+                }}
             >
                 {(removeFromCart, {loading, error}) => (
                     <BigButton title="Delete Item" disabled={loading} onClick={() => {
