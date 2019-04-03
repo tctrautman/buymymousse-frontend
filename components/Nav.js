0f7333ebc16a635103cfcfb2365b357f538a1f2f
tctrautman <tctrautman@gmail.com>
2019-04-03 14:11:36 -0700

    Keep a tally of the number of items in cart, in Nav.  Add animations

--- components/Nav.js
+++ components/Nav.js
@@ -4,6 +4,7 @@ import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
 import { TOGGLE_CART_MUTATION } from './Cart';
+import CartCount from './CartCount';
 
 const Nav = () => (
     <User>
@@ -28,6 +29,9 @@ const Nav = () => (
                             {(toggleCart) => (
                                 <button onClick={toggleCart}>
                                     My Cart
+                                    <CartCount count={me.cart.reduce(
+                                        (tally, cartItem) => tally + cartItem.quantity, 0
+                                    )}/>
                                 </button>
                             )}
                         </Mutation>
