094388c1906d52a91ad7b2046cfa53ecd25c4b8a
tctrautman <tctrautman@gmail.com>
2019-04-03 20:16:01 -0700

    Add a single order page

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -31,6 +31,7 @@ let totalItems = (cart) => {
 
 class TakeMyMoney extends React.Component {
     onToken = async (res, createOrder) => {
+        NProgress.start();
         const order = await createOrder({
             variables: {
                 token: res.id
@@ -38,7 +39,10 @@ class TakeMyMoney extends React.Component {
         }).catch(err => {
             alert(err.message);
         });
-        console.log(order)
+        Router.push({
+            pathname: '/order',
+            query: { id: order.data.createOrder.id }
+        })
     };
 
     render() {
